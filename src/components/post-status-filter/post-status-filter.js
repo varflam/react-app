1ab8aecb6c56d16016d2d1ab60c13b18f7b5b5b4
Maria <alece.1414@gmail.com>
2021-02-06 13:38:14 +0300

    Button fix

--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -3,8 +3,8 @@ import React from 'react';
 const PostStatusFilter = () => {
     return (
         <div className="btn-group">
-            <button></button>
-            <button></button>
+            <button className="btn btn-outline-info">Все</button>
+            <button className="btn btn-outline-secondary">Понравилось</button>
         </div>
     )
 }
